0574a7beef5dc76cd015354fbe87930640010feb
Sylvia Lee <sylvialee19@hotmail.com>
2019-02-08 22:14:07 -0800

    added init and setup scripts to root directory

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='normtestPY',
+    version='0.1.0',
+    author= ['Constantin Shuster', 'Sylvia Lee', 'Richie Zitomer'],
+    packages=['normtestPY'],
+    license='MIT',
+    description='This is a Python package that tests your data for normality!',
+    url='https://github.com/UBC-MDS/normtestPY',
+    install_requires=[]
+)
