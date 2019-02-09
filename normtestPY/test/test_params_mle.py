6a4ef99f7363d8206563e5da5ccee66ef57fa094
Sylvia Lee <sylvialee19@hotmail.com>
2019-02-09 15:33:13 -0800

    finished unit tests for python params_mle function

--- normtestPY/test/test_params_mle.py
+++ normtestPY/test/test_params_mle.py
@@ -13,34 +13,39 @@ import pandas as pd
 import numpy as np
 from normtestPY.params_mle import params_mle
 
-# Dummy data for testing
+# Dummy data for testing in different datatypes
 dummy = pd.DataFrame({"var1": [0,1,1,-1], "var2": [-1, -1, 0, 1]})
-expected = pd.DataFrame({"var1": [1/4, (11/16)**(1/3)], "var2": [2,3]})
+series_type = (dummy["var1"])
+matrix_type = np.array(dummy)
+list_2d_type = list([list(dummy["var1"]), list(dummy["var2"])])
+
+# Expected output from Dummy
+expected = pd.DataFrame({"var1": [1/4, (11/16)**0.5], "var2": [-1/4, (11/16)**0.5]})
 
 # Unit tests
-
-# Test for the most common case where input is a df
-# Check for correct output type, and calculations
-def test_params_mle():
+def test_params_mle_calc():
     results = params_mle(dummy)
-
-    assert isinstance(params_mle(dummy), pd.DataFrame)
     assert np.allclose(results, expected)
-
-# Test for other possible input types
-def test_non_df_inputs():
-    series_type = (dummy["var1"])
-    matrix_type = np.array(dummy)
-    list_2d_type = list([list(dummy["var1"]), list(dummy["var2"])])
-
     assert np.allclose(params_mle(series_type), expected["var1"])
     assert np.allclose(params_mle(matrix_type), expected)
     assert np.allclose(params_mle(list_2d_type), expected)
+
+def test_valid_non_df_inputs():
+    assert isinstance(params_mle(dummy), pd.DataFrame)
     assert isinstance(params_mle(matrix_type), pd.DataFrame)
     assert isinstance(params_mle(list_2d_type), pd.DataFrame)
     assert isinstance(params_mle(series_type, pd.DataFrame))
 
-# Test for empty inputs
-def test_empty inputs():
-    
-# Exception handling
+def test_empty_inputs():
+    with pytest.raises(AttributeError):
+        params_mle(pd.DataFrame({"var1": [], "var2": []}))
+    with pytest.raises(AttributeError):
+        params_mle(list())
+
+def test_non_numeric_inputs():
+    with pytest.raises(TypeError):
+        params_mle(["male", "female", "male", "female"])
+    with pytest.raises(TypeError):
+        params_mle(pd.DataFrame({"var1": [1,2,"5","9"]}))
+    with pytest.raises(TypeError):
+        params_mle([True, False, True])
