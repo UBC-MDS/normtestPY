84bcda0764f3b4a17a23fef38aa7897bfa63fb4c
rzitomer <richiezitomer@gmail.com>
2019-02-09 16:48:58 -0800

    Add in additional tests for our functions

--- normtestPY/make_qqplot.py
+++ normtestPY/make_qqplot.py
@@ -2,11 +2,11 @@ def make_qqplot(data):
     '''
     Function: Create QQ-plot for each continuous variable in the data
 
-    Input:    DATAFRAME where data for each continous variable is in its
-              respective column
+    Input:    DATAFRAME (where data for each continous variable is in its
+                respective column), LIST, ARRAY, or SERIES
 
     Output:   DICTIONARY of plots kere the keys are the column name (or index)
     '''
     print("make_qqplot - empty function")
 
-    return()
+    pass
--- normtestPY/test/test_make_qqplot.py
+++ normtestPY/test/test_make_qqplot.py
@@ -2,12 +2,72 @@
 This script tests the function from make_qqplot.py
 
 The function creates QQ-plot for each continuous variable in the data
-# Input:  DATAFRAME where data for each continous variable is in its
-          respective column
-# Output: DICTIONARY of plots kere the keys are the column name (or index)
+# Input:  DATAFRAME (where data for each continous variable is in its
+          respective column), LIST, ARRAY, or SERIES
+# Output: DICTIONARY of plots where the keys are the column name (if passed a dataframe or series) or are the index
+  (if passed a list or array)
 '''
 
-import pytest
+
+import numpy as np
+import pandas as pd
+
 from normtestPY.make_qqplot import make_qqplot
 
-# Unit Tests
+
+# Sample data
+data = [1, 2, 3]
+data_array = np.array([[1, 2], [3, 4]])
+data_series = pd.Series({'a':[1,2,3]})
+data_df = pd.DataFrame({'a': [1, 2, 3], 'b': [2, 3, 4]})
+
+
+def test_output():
+    """Check that output is a dict."""
+    plots = make_qqplot(data)
+    assert type(plots) == dict
+
+
+def test_var_length_list():
+    """Check that length of dict == 1 when passed list."""
+    plots = make_qqplot(data)
+    assert len(plots.keys()) == 1
+
+
+def test_var_length_array():
+    """Check that length of dict <= length of columns in an array when passed an array."""
+    plots = make_qqplot(data_array)
+    assert len(plots.keys()) <= data_array.shape[1]
+
+
+def test_var_length_df():
+    """Check that length of dict <= length of columns when passed a dataframe."""
+    plots = make_qqplot(data_df)
+    assert len(plots.keys()) <= len(data_df.columns)
+
+
+def test_var_length_series():
+    """Check that length of dict == 1 when passed a series."""
+    plots = make_qqplot(data_series)
+    assert len(plots.keys()) == 1
+
+
+def test_plots():
+    """Check that each element is a plot"""
+    plots = make_qqplot(data)
+    for key in plots.keys():
+        assert isinstance(plots[key][0], matplotlib.lines.Line2D)
+
+def test_key_names_array():
+    """Check key names for an array-like object"""
+    plots = make_qqplot(data_array)
+    for n, key in enumerate(plots.keys()):
+        assert n == key
+
+
+def test_key_names_df():
+    """Check key names for a df-like object"""
+    plots = make_qqplot(data_df)
+    for key in plots.keys():
+        assert key in data_df.columns
+
