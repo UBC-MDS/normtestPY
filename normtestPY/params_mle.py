6b9d13a9f64c06c1d4da6108e82c3ba290a04aa9
rzitomer <richiezitomer@gmail.com>
2019-02-09 17:46:53 -0800

    Update documentation

--- normtestPY/params_mle.py
+++ normtestPY/params_mle.py
@@ -1,14 +1,20 @@
 def params_mle(data):
     '''
-    Function: Fit data to a Guassian distribution with Maximum Likelihood
-              Estimation (MLE)
+    Fit data to a Guassian distribution with Maximum Likelihood Estimation (MLE)
 
-    Input:    DATAFRAME where data for each continous variable is in its
+    Parameters
+    ----------
+    DATAFRAME where data for each continous variable is in its
               respective column
 
-    Output:   DATAFRAME where the first row contains the estimated means and
+    Returns
+    -------
+    DATAFRAME where the first row contains the estimated means and
               the second row contains the estimated variance, and the columns
               present the original variables in the data
+
+    Examples
+    --------
     '''
     print("params_mle - empty function")
 
