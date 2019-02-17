d9b0ca4ecf476b78ffdf25d76800ca3dd63c7737
rzitomer <richiezitomer@gmail.com>
2019-02-17 09:42:43 -0800

    Update make_qqplot based on code review

--- normtestPY/make_qqplot.py
+++ normtestPY/make_qqplot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def make_qqplot(data):
+def make_qqplot(data, print_plots = True):
     '''
     Create QQ-plot for each continuous variable in the data
 
@@ -10,6 +10,10 @@ def make_qqplot(data):
     -------
     data : ndarray, list, dict, or DataFrame
         Data to be tested for normality
+
+    print_plots: Boolean
+        Toggle whether or not the plots are interactively printed out while
+        they're being created. Default is True.
 
     Returns
     -------
@@ -43,10 +47,14 @@ def make_qqplot(data):
 
     elif isinstance(data, list):
         var_names = range(len(data))
-        data = np.array(data)
+        data = np.transpose(np.array(data))
         data = data[:,None]
 
     else:
+        raise TypeError
+
+    # Exception handling if array element is not numeric
+    if data.dtype.kind not in ["i", "u", "f", "c"]:
         raise TypeError
 
     q = np.linspace(0,1,20) # Sequence of quantiles
@@ -59,6 +67,12 @@ def make_qqplot(data):
         ax.scatter(x, y)
         plt.xlabel('Theoretical Distribution')
         plt.ylabel('Sample Distribution')
+        if print_plots:
+            plt.show()
+        elif not print_plots:
+            plt.close()
+        else:
+            raise TypeError
         plots[var_names[n]] = fig
 
     return plots
